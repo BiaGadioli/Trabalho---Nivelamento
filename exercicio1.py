77b4df5fa96c18a4a349551928e59adabfed5bb9
BiaGadioli <167255103+BiaGadioli@users.noreply.github.com>
2025-02-20 11:32:09 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+#Altura das pessoas do genero masculino e feminino, 15 pessoas. 
+# A maior aultura do grupo; A média de altura das pessoas do gênero masculino; O número de pessoas do gênero feminino.
+
+dados = [
+    {""}
+]
